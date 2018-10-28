48958532adc6bc5b5c44b5da99413ef3323b1229
min <mjnorthwesternwork1121@gmail.com>
2018-10-27 23:45:03 -0500

    Added buttons on html and syntax on js file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,47 @@
-// ALERT USER 
+// Welcome user alert
 alert (" Welcome to 'Crystals Collector'!!")
 
-//1. The player will have to guess the answer, just like in Word Guess. This time, though, the player will guess with numbers instead of letters. 
+//The player will have to guess the answer. The player will guess with numbers instead of letters. 
 
-//3. Here's how the app works:
+  // * There will be four crystals displayed as buttons on the page. 'c' stands for crystal.
 
-  // * There will be four crystals displayed as buttons on the page.
+    var crystalList = ["c1", "c2", "c3","c4"];
+
+    console.log = ["crystalList"]
 
    //* The player will be shown a random number at the start of the game.
 
+   $(document).ready(function() {
+
+    $("#random-button").on("click", function() {
+
+      // Create a string which will hold the lottery number
+      var crystalNumber = "";
+
+      // Then initiate a loop to generate 9 separate numbers
+      for (var i = 0; i < 9; i++) {
+
+        // For each iteration, generate a new random number between 1 and 9.
+        var random = Math.floor(Math.random() * 9) + 1;
+
+        // Take this number and then add it to the rest of the string.
+        // In essence, we are iteratively building a string of numbers. (e.g. First: 1, Second: 13, Third: 135, etc.)
+        CrytalNumber = random + crystalNumber;
+
+      }
+
+      // ... and then dump the random number into our random-number div.
+      $("#random-number").prepend("<br><hr>" + CrystalNumber);
+
+    });
+
+  });
+
    //* When the player clicks on a crystal, it will add a specific amount of points to the player's total score. 
 
-     //* Your game will hide this amount until the player clicks a crystal.
-     //* When they do click one, update the player's score counter.
+   //* Your game will hide this amount until the player clicks a crystal.
+  
+   //* When they do click one, update the player's score counter.
 
    //* The player wins if their total score matches the random number from the beginning of the game.
 
